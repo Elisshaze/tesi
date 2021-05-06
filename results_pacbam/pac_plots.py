ebb68b3fbbcd3dde0916cf1827825b173f318b4e
Elisshaze <elisa.pettina@studenti.it>
2021-05-06 14:20:16 +0200

    dsv

--- results_pacbam/pac_plots.py
+++ results_pacbam/pac_plots.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 from upsetplot import from_memberships,plot,from_contents
 import itertools
 
-def buildSets(globPath, minCov, minAF, maxAF):
+out_pac = "/GROUPS/sharedRL/tirocinanti/elisa/out_pacbam"
+
+def buildSets(globPath, minCov, minAF, maxAF, extention):
     sets = {}
     for f in glob.glob(globPath):
         fname = f.split("/")[2].split(".")[0].split("_")[0:3]
@@ -16,8 +18,13 @@ def buildSets(globPath, minCov, minAF, maxAF):
             for l in pileup:
                 l = l.strip().split()
                 rsid = l[2]
-                cov = int(l[16])
-                af = float(l[15])
+                cov = int(l[10])
+				af = float(l[9])
+				if extention == "pabs" :
+					rsid = l [1]
+					cov = int(l[9])
+					af = int(l[8])
+	
                 if cov >= minCov and af >= minAF and af <= maxAF:
                     sets[fname].add(rsid)
     return sets
@@ -37,22 +44,20 @@ def unionSubsetCarbon(subset):
 
 minCov = [10,20]
 af = [(0.1,0.9),(0.2,0.8)]
+ex = [snps, pabs]
 
-
-
-
-for cov, afRange in itertools.product(minCov,af):
+for cov, afRange, extention in itertools.product(minCov,af, ex):
     afMin = afRange[0]
     afMax = afRange[1]
 
-    setsCarbon = buildSets("../out_aseq/Carbon*.ASEQ",cov,afMin,afMax)
-    plotUpset(setsCarbon,f'./images/Carbon_{cov}_{afMin}_{afMax}.png')
-    setsMock = buildSets("../out_aseq/mock*.ASEQ",cov,afMin,afMax)
-    plotUpset(setsMock,f'./images/mock_{cov}_{afMin}_{afMax}.png')
-    setsXray = buildSets("../out_aseq/X-ray*.ASEQ",cov,afMin,afMax)
-    plotUpset(setsXray,f'./images/X-ray_{cov}_{afMin}_{afMax}.png')
-    setsProton = buildSets("../out_aseq/Proton*.ASEQ",cov,afMin,afMax)
-    plotUpset(setsProton,f'./images/Proton_{cov}_{afMin}_{afMax}.png')
+    setsCarbon = buildSets(out_pac+"/Carbon*."+extention,cov,afMin,afMax,extention)
+    plotUpset(setsCarbon,f'./images/Carbon_{cov}_{afMin}_{afMax}_{extention}.png')
+    setsMock = buildSets(out_pac+"/mock*."+extention,cov,afMin,afMax,extention)
+    plotUpset(setsMock,f'./images/mock_{cov}_{afMin}_{afMax}_{extention}.png')
+    setsXray = buildSets(out_pac+"/X-ray*."+extention,cov,afMin,afMax,extention)
+    plotUpset(setsXray,f'./images/X-ray_{cov}_{afMin}_{afMax}_{extention}.png')
+    setsProton = buildSets(out_pac+"/Proton*"+extention,cov,afMin,afMax,extention)
+    plotUpset(setsProton,f'./images/Proton_{cov}_{afMin}_{afMax}_{extention}.png')
 
     setTotal = {}
     setTotal["Carbon"] = unionSubset(setsCarbon)
@@ -60,7 +65,7 @@ for cov, afRange in itertools.product(minCov,af):
     setTotal["X-ray"] = unionSubset(setsXray)
     setTotal["mock"] = unionSubset(setsMock)
 
-    plotUpset(setTotal, f'./images/allConditions_{cov}_{afMin}_{afMax}.png')
+    plotUpset(setTotal, f'./images/allConditions_{cov}_{afMin}_{afMax}_{extention}.png')
 
     setTotal["Carbon"] = unionSubsetCarbon(setsCarbon)
-    plotUpset(setTotal, f'./images/allConditions_CarbonTOT2Removed_{cov}_{afMin}_{afMax}.png')
+    plotUpset(setTotal, f'./images/allConditions_CarbonTOT2Removed_{cov}_{afMin}_{afMax}_{extention}.png')
